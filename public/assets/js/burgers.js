e1835d29f00d40de97b75dfe7bca222b84ea9f02
Kaylie V <k.verner37@gmail.com>
2020-05-28 18:15:18 -0400

    updating burgers.js

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -2,7 +2,7 @@
 $(function() {
     $(".change-devour").on("click", function(event) {
       var id = $(this).data("id");
-      var newDevour = $(this).data("newdevour");
+      var newDevour = false;
   
       var newDevourState = {
         devoured: newDevour
@@ -27,7 +27,7 @@ $(function() {
   
       var newBurger = {
         burger_name: $("#ca").val().trim(),
-        // devoured: $("[burger_name=devoured]:checked").val().trim()
+        devoured: 0
       };
   
       // Send the POST request.
